e7e88ce4d3c32c3a04f63654d4a603e2f8d07a42
AdminTozbos <momcilovic.vesna@gmail.com>
2025-01-30 14:25:52 +0100

    Dodata login forma

--- src/form/LoginForma.java
+++ src/form/LoginForma.java
@@ -4,14 +4,21 @@
  */
 package form;
 
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JOptionPane;
 import komunikacija.Komunikacija;
+import model.KlijentskiZahtev;
+import model.Operacije;
+import model.RukovodilacKooperacije;
+import model.ServerskiOdgovor;
 
 /**
  *
  * @author Miloš
  */
 public class LoginForma extends javax.swing.JFrame {
-
+    int loginBrojac=3;
     /**
      * Creates new form LoginForma
      */
@@ -19,7 +26,16 @@ public class LoginForma extends javax.swing.JFrame {
         initComponents();
         Komunikacija k=Komunikacija.getInstance();
     }
-
+    public RukovodilacKooperacije prijaviRukovodilacKooperacije(String korisnickoIme,String sifra){
+        List<String>parametri=new ArrayList<>();
+        parametri.add(korisnickoIme);
+        parametri.add(sifra);
+        KlijentskiZahtev kz=new KlijentskiZahtev(Operacije.LOGIN, parametri);
+        Komunikacija.getInstance().posaljiZahtev(kz);
+        ServerskiOdgovor so=Komunikacija.getInstance().primiOdgovor();
+        RukovodilacKooperacije r=(RukovodilacKooperacije) so.getOdgovor();
+        return r;
+    }
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -29,21 +45,87 @@ public class LoginForma extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jTextField1 = new javax.swing.JTextField();
+        jPasswordField1 = new javax.swing.JPasswordField();
+        jButton1 = new javax.swing.JButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel1.setText("Korisnicko ime");
+
+        jLabel2.setText("Lozinka");
+
+        jButton1.setText("Uloguj se");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 90, Short.MAX_VALUE)
+                    .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jTextField1)
+                    .addComponent(jPasswordField1, javax.swing.GroupLayout.DEFAULT_SIZE, 149, Short.MAX_VALUE))
+                .addContainerGap(149, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jButton1)
+                .addGap(52, 52, 52))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(31, 31, 31)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel1)
+                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel2)
+                    .addComponent(jPasswordField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(48, 48, 48)
+                .addComponent(jButton1)
+                .addContainerGap(124, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        String username=jTextField1.getText();
+        char[]pass=jPasswordField1.getPassword();
+        String password="";
+        for (char pas : pass) {
+            password=password+pas+"";
+        }
+        RukovodilacKooperacije r=prijaviRukovodilacKooperacije(username, password);
+        if(r==null){
+            if(loginBrojac==1){
+                JOptionPane.showMessageDialog(this, "Nemate vise pokusaja","Obavestenje",JOptionPane.INFORMATION_MESSAGE);
+                this.dispose();
+                return;
+            }
+            JOptionPane.showMessageDialog(this, "Parametri za prijavu nisu tacni\n"+"Ostalo vam je jos "+(--loginBrojac)+" pokusaja", "Greska", JOptionPane.ERROR_MESSAGE);
+            
+        }
+        else{
+           JOptionPane.showMessageDialog(this, "Uspesno ste se prijavili");
+           KlijentskaForma kf=new KlijentskaForma(r);
+           kf.setVisible(true);
+           this.dispose();
+        }
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -81,5 +163,10 @@ public class LoginForma extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JPasswordField jPasswordField1;
+    private javax.swing.JTextField jTextField1;
     // End of variables declaration//GEN-END:variables
 }
